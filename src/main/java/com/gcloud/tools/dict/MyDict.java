07d144eb63cd9358d5ec48652d9646be375f6412
mocanjie <canjiemo@gmail.com>
2021-08-02 09:48:00 +0800

    fix

--- src/main/java/com/gcloud/tools/dict/MyDict.java
+++ src/main/java/com/gcloud/tools/dict/MyDict.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 public @interface MyDict {
     String name();
     String defaultDesc() default "";
