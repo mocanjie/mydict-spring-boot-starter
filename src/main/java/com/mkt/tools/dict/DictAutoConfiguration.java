e2e218b99f5b3775f0acf41b401a0d98efbb169b
mocanjie <canjiemo@gmail.com>
2021-08-25 17:14:00 +0800

    fix config

--- src/main/java/com/mkt/tools/dict/DictAutoConfiguration.java
+++ src/main/java/com/mkt/tools/dict/DictAutoConfiguration.java
@@ -1,11 +1,9 @@
 package com.mkt.tools.dict;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnWebApplication
 public class DictAutoConfiguration {
 
     @Bean
